9541a9dc1f41a827a824b4c04a56be5032c7df26
MagikSquirrel <virgil_90210@yahoo.com>
2017-07-17 19:56:53 -0400

    Finish off test data...
    We now have 3 users with subscriptions using various methods.

--- createDB.rb
+++ createDB.rb
@@ -59,14 +59,35 @@ SQL
 
 # Insert starting data
 users = ["userA@company1.com", "userB@company2.com","userC@company1.com"]
+processors = [["Visa", "Credit"],["MasterCard", "Credit"],["Discover", "Credit"],["AmericanExpress", "Credit"],["Paypal", "Online"]]
+intervals = [["annual", 10000], ["monthly", 1000]]
+subscriptions = [[1, 1, 1, 1500329574, 1600329574], [2,2,2,1490329574,1510329574],[3,5,1,1490329574,1510329574]]
 
 users.each do |user|
   db.execute("INSERT INTO user VALUES (null, ?);", user)
 end
 
+processors.each do |processor|
+  db.execute("INSERT INTO processor VALUES (null, ?,?);", processor)
+end
+
+intervals.each do |interval|
+  db.execute("INSERT INTO pmt_interval VALUES (null, ?,?);", interval)
+end
+
+subscriptions.each do |subs|
+  db.execute("INSERT INTO subscription VALUES (null,?,?,?,?,?,null);", subs)
+end
+
 # Find a few rows
 sqlSelect = <<-SQL
-  SELECT * FROM user;
+  SELECT u.email, i.type, p.name, p.type,
+    strftime("%m-%d-%Y", lastPayment, 'unixepoch') AS lastPayment,
+    strftime("%m-%d-%Y", expiration, 'unixepoch') AS expiration
+  FROM subscription AS pu
+    INNER JOIN user AS u ON u.id = pu.user_id
+    INNER JOIN processor AS p ON p.id = pu.processor_id
+    INNER JOIN pmt_interval AS i ON i.id = pu.pmt_interval_id
 SQL
 db.execute( sqlSelect ) do |row|
   p row
