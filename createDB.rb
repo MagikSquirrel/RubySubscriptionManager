53ed0f596b2f88d8ddb67d256d26f7aae2541a24
MagikSquirrel <virgil_90210@yahoo.com>
2017-07-17 19:34:09 -0400

    Payment intervals, Subscription
    Payment Intervals is for annual/monthly.
    
    Subscription maps users/payments/intervals as a subscription.

--- createDB.rb
+++ createDB.rb
@@ -16,3 +16,22 @@ rows = db.execute <<-SQL
     type varchar(16)
   );
 SQL
+
+rows = db.execute <<-SQL
+  CREATE TABLE pmt_interval (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    type varchar(8),
+    cents int
+  );
+SQL
+
+rows = db.execute <<-SQL
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id int,
+    processor_id int,
+    pmt_interval_id int,
+    lastPayment int,
+    expiration int,
+    json_processor_data BLOB
+  ); 
+SQL
