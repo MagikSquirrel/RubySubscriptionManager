53963b6fbd41215984a30035124aaa3ebe05898e
MagikSquirrel <virgil_90210@yahoo.com>
2017-07-17 20:15:22 -0400

    Fleshing out enums/variables that I'll need for the Charger

--- subscriptionManager.rb
+++ subscriptionManager.rb
@@ -1,13 +1,30 @@
 #!/usr/bin/env ruby -w
 
 #Class that reaches out to the processor's payment network to process payments
-class Charger
-
+class  @@method
+  @@cents #Use cents to not have to deal with floats
+  
+  def method
+    puts method
+  end
+  
+  def cents
+    puts cents
+  end
+  
+  #Various Transaction Types
+  enum TX_TYPES: [
+    SIGNUP,
+    RENEWAL,
+    FAILED_CHARGE
+  ]
 end
 
 #Class that associates with a payment provider
 class Provider
-
+  @@name
+  @@type
+  @@apiUrl
 end
 
 #Class that handles the mailing operations
