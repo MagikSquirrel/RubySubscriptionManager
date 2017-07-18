eb6f98a03b41e01bcc387d764e00e5f6bc62b724
MagikSquirrel <virgil_90210@yahoo.com>
2017-07-17 20:00:04 -0400

    Class files for the cron job
    Charger: What handles processing of payments
    Provider: A relation to the DB provider
    Mailer: Sends the emails based on success/failed payment
    User: A relation to the DB user

---
+++ subscriptionManager.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby -w
+
+#Class that reaches out to the processor's payment network to process payments
+class Charger
+
+end
+
+#Class that associates with a payment provider
+class Provider
+
+end
+
+#Class that handles the mailing operations
+class Mailer 
+
+end
+
+class User
+
+end
+
