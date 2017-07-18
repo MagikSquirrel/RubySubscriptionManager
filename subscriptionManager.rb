025109843d98a82f01f23adee60b2e819d3525d9
MagikSquirrel <virgil_90210@yahoo.com>
2017-07-17 20:23:51 -0400

    Fleshing out enums/variables that I'll need for the Mailer
    Rely on replacement strings to populate fields in email

--- subscriptionManager.rb
+++ subscriptionManager.rb
@@ -28,7 +28,21 @@ class Provider
 end
 
 #Class that handles the mailing operations
-class Mailer 
+class Mailer
+  @@sender
+  @@recipient
+  @@subject
+  
+  #Mailer specific enums
+  enum SUBJECTS: {
+    SUCCESS: "Your payment was successful",
+    FAILURE: "Your payment was UNsuccessful."
+  }
+  
+  enum MESSAGES: {
+    SUCCESS: "Congratulations you\'ve successfully paid your [post]interval[/post] bill for service totaling [post]amount[/post] with payment method [post]method[/post] indentified by [post]account[/post].",
+    FAILURE: "Unfortunately the payment method [post]method[/post] identified by [post]account[/post] was unsuccessful for [post]amount[/post] for your [post]interval[/post] payment. Please correct payment or contact our support at [post]supportEmail[/post]"
+  }
 
 end
 
